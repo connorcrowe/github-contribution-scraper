7757a17c460058b36ce5681af3d62338d27210ad
connorcrowe <connor.haz.crowe@gmail.com>
2022-10-05 19:49:21 -0400

    Add scrape of github contributions into js object from a user URL

--- get-gh.js
+++ get-gh.js
@@ -1,2 +1,23 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
+const axios = require('axios');
+const cheerio = require('cheerio');
+const pretty = require("pretty");
+
+const url = ''
+
+async function scrapeData() {
+    try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+        let contribs = {}
+
+        let listContributionDays = $(".ContributionCalendar-day");
+        for (let i=0; i<listContributionDays.length; i++) {
+            contribs[listContributionDays[i]['attribs']['data-date']] = listContributionDays[i]['attribs']['data-level'];
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+scrapeData();
+console.log('done')
